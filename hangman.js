4ab6f3b360a26336c8c027b17a1fdd2e80fb31f6
PantaZvaniPub <pantelicrade@gmail.com>
2018-11-06 15:09:17 +0100

    works, letters and click listeners added

--- hangman.js
+++ hangman.js
@@ -31,34 +31,64 @@ function gameStart() {
   };
 };
 
+//making letters on html and adding events
+for(y=0; y < misteryMovie.length; y++){
+  if(misteryMovie[y] === " "){
+    document.querySelector("#mysteryMovie").innerHTML+= "<div class=\"space\"> </div>";
+  } else {
+  document.querySelector("#mysteryMovie").innerHTML+= "<div class=\"letter\">*</div>";
+  };
+};
+
+//ako ubacim jquery moze ovako
+// $(".letter").on("click", function() {this.textContent = "aa"});
+
+
+var lett = document.querySelectorAll(".letter")
+for( u=0; u<misteryMovie.length-1; u++){
+  lett[u].addEventListener("click", function() {
+    if (this.textContent === "*"){
+      this.textContent = bestLetter;
+    } else if(this.textContent === bestLetter){
+      this.textContent = "*";
+    };
+  });
+}
+
+
+function bestLetterFinder() {
+  //reset values
+  bestLetter = letters[0];
+  moviesWithBestLetter = 0;
+  //cycling through the letters
+  for (a = 0; a < letters.length; a++){
+    currentLetter = letters[a];
+    //cycling through the movies to check if they have the letter
+    for(b = 0; b < potentialMovies.length; b++){
+      if( potentialMovies[b].indexOf(letters[a]) != -1){
+        moviesWithCurrentLetter = moviesWithCurrentLetter + 1;
+      };
+    };
+    //if this letter is better to guess then use it
+    if( Math.abs(moviesWithCurrentLetter - potentialMovies.length/2) < Math.abs(moviesWithBestLetter - potentialMovies.length/2) ) {
+      bestLetter = currentLetter;
+      moviesWithBestLetter = moviesWithCurrentLetter;
+    };
+    //reset for the next letter
+    moviesWithCurrentLetter = 0;
+  };
+  //remove the letter so we dont guess it again
+  letters.splice(letters.indexOf(bestLetter), 1);
+};
+
 // If there are stars in the misteryMovie title, keep guessing
 while ( misteryMovie.indexOf("*") !== -1) {
-  game() };
+  game()
+};
 
 // Basic game guessing function
   function game(){
-    //reset values
-    bestLetter = letters[0];
-    moviesWithBestLetter = 0;
-    //cycling through the letters
-    for (a = 0; a < letters.length; a++){
-      currentLetter = letters[a];
-      //cycling through the movies to check if they have the letter
-      for(b = 0; b < potentialMovies.length; b++){
-        if( potentialMovies[b].indexOf(letters[a]) != -1){
-          moviesWithCurrentLetter = moviesWithCurrentLetter + 1;
-        };
-      };
-      //if this letter is better to guess then use it
-      if( Math.abs(moviesWithCurrentLetter - potentialMovies.length/2) < Math.abs(moviesWithBestLetter - potentialMovies.length/2) ) {
-        bestLetter = currentLetter;
-        moviesWithBestLetter = moviesWithCurrentLetter;
-      };
-      //reset for the next letter
-      moviesWithCurrentLetter = 0;
-    };
-    //remove the letter so we dont guess it again
-    letters.splice(letters.indexOf(bestLetter), 1);
+    bestLetterFinder()
     bestLetterPositions = prompt("Is there a letter: \n" + bestLetter + "\nand in what positions (ex. 3,11,15). If there are none, write \"n\"");
     //if mistery movie doesn't have the letter remove all the movies that have it
     if(bestLetterPositions === "n"){
